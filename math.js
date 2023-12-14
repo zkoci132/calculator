8aa96d9660947e0a8ce3c97e1da5803132c1c5e5
zkoci132 <zkoci132@csitrd.kutztown.edu>
2023-12-14 03:46:01 -0500

    created outline for calculator in html

---
+++ math.js
@@ -0,0 +1,24 @@
+let lhs = 
+
+
+
+function add(num1,num2){
+    return num1 + num2
+}
+
+
+function sub(num1,num2){
+    return num1 - num2
+}
+
+
+function mult(num1,num2){
+    return num1 * num2
+}
+
+
+function div(num1,num2){
+    return num1/num2
+}
+
+
